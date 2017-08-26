4aa3dcc3ff4f9fb8ea53a627775bfd1c928abe47
angelchanquin <ajchanquin@gmail.com>
2017-08-25 23:13:11 -0600

    fix drawables on android 4

--- src/BackupAppAndroid/app/src/main/java/com/americavoice/backup/AndroidApplication.java
+++ src/BackupAppAndroid/app/src/main/java/com/americavoice/backup/AndroidApplication.java
@@ -1,16 +1,13 @@
 
 package com.americavoice.backup;
 
-import android.app.Application;
-import android.content.ComponentName;
 import android.content.Context;
-import android.content.ServiceConnection;
 import android.content.SharedPreferences;
 import android.content.res.Resources;
 import android.os.Environment;
-import android.os.IBinder;
 import android.support.multidex.MultiDex;
 import android.support.multidex.MultiDexApplication;
+import android.support.v7.app.AppCompatDelegate;
 
 import com.americavoice.backup.datamodel.ThumbnailsCacheManager;
 import com.americavoice.backup.db.PreferenceManager;
@@ -19,14 +16,20 @@ import com.americavoice.backup.di.components.DaggerApplicationComponent;
 import com.americavoice.backup.di.modules.ApplicationModule;
 import com.americavoice.backup.service.NCJobCreator;
 import com.americavoice.backup.utils.BaseConstants;
-import com.evernote.android.job.JobManager;
 import com.crashlytics.android.Crashlytics;
+import com.crashlytics.android.core.CrashlyticsCore;
+import com.evernote.android.job.JobManager;
+
 import io.fabric.sdk.android.Fabric;
 
 /**
  * Android Main Application
  */
 public class AndroidApplication extends MultiDexApplication {
+
+    static {
+        AppCompatDelegate.setCompatVectorFromResourcesEnabled(true);
+    }
 
     private ApplicationComponent applicationComponent;
     private static Context mContext;
@@ -52,8 +55,9 @@ public class AndroidApplication extends MultiDexApplication {
         // initialise thumbnails cache on background thread
         new ThumbnailsCacheManager.InitDiskCacheTask().execute();
 
-//        Fabric.with(this, new Crashlytics());
-        //Debug Fabric.with(this, new Crashlytics.Builder().core(new CrashlyticsCore.Builder().disabled(BuildConfig.DEBUG).build()).build());
+        Fabric.with(this, new Crashlytics());
+//        Debug
+        Fabric.with(this, new Crashlytics.Builder().core(new CrashlyticsCore.Builder().disabled(BuildConfig.DEBUG).build()).build());
 
 
         SharedPreferences appPrefs =
